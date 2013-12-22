3bb49713216c9cd3dbe29a36e42bf2ac0fc21799
Daniel Wright <daniel.layton.wright@gmail.com>
2013-12-22 14:56:51 -0700

    Created Dictionary class, with contains and containsSubstring methods.

--- src/main/java/com/wright/crypto/Controller.java
+++ src/main/java/com/wright/crypto/Controller.java
@@ -6,6 +6,8 @@ package com.wright.crypto;
  */
 public class Controller {
 
+    public static void main(String[] args) {
 
+    }
 
 }
---
+++ src/main/java/com/wright/crypto/View.java
@@ -0,0 +1,10 @@
+package com.wright.crypto;
+
+/**
+ * @author Danny
+ * @date 12/21/13 6:26 PM
+ */
+public interface View {
+    String getPuzzleText();
+
+}
