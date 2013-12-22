3d123cae10e2d61668a8179f856577cdd59af39e
Daniel Wright <daniel.layton.wright@gmail.com>
2013-12-21 17:39:07 -0700

    Frequency test.

--- src/test/java/com/wright/crypto/SolverTest.java
+++ src/test/java/com/wright/crypto/SolverTest.java
@@ -2,6 +2,9 @@ package com.wright.crypto;
 
 
 import org.junit.Test;
+
+import java.util.HashMap;
+
 import static org.junit.Assert.*;
 
 public class SolverTest {
@@ -10,5 +13,15 @@ public class SolverTest {
     public void testSolver() {
         Solver solver = new Solver("ij uifsf");
         assertEquals("hi there", solver.solve());
+    }
+
+    @Test
+    public void frequencyTest1() {
+        Solver solver = new Solver("ABCABA");
+        HashMap<Character, Integer> frequencies = solver.getFrequencies();
+        assertEquals(3, (int)frequencies.get(new Character('A')));
+        assertEquals(2, (int)frequencies.get(new Character('B')));
+        assertEquals(1, (int)frequencies.get(new Character('C')));
+
     }
 }
