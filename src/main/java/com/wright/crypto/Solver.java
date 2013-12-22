8b73ceb0220e5f446919682f138ccef8fa3f7890
Daniel Wright <daniel.layton.wright@gmail.com>
2013-12-21 17:45:51 -0700

    Wrote getFrequencies method.

--- src/main/java/com/wright/crypto/Solver.java
+++ src/main/java/com/wright/crypto/Solver.java
@@ -1,13 +1,31 @@
 package com.wright.crypto;
 
 
+import java.util.HashMap;
+
 public class Solver {
 
-    public Solver(String cipherText) {
+    String puzzleText;
 
+    public Solver(String encryptedText) {
+        puzzleText = encryptedText.toUpperCase(); //Use all uppercase letters
     }
 
     public String solve() {
         return "hi there";
+    }
+
+    HashMap<Character, Integer> getFrequencies() {
+        HashMap<Character, Integer> frequencies = new HashMap<Character, Integer>();
+        for (int i = 0; i < puzzleText.length(); i++) {
+            Character c = new Character(puzzleText.charAt(i));
+            if (frequencies.containsKey(c)) {
+                frequencies.put(c, frequencies.get(c) + 1);
+            } else {
+                frequencies.put(c, 1);
+            }
+        }
+
+        return frequencies;
     }
 }
